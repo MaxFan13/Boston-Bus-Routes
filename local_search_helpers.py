659bd8e0a864def8f5fc718077ce1a75c7d94602
wcohen23 <112724028+wcohen23@users.noreply.github.com>
2025-06-11 17:33:10 -0400

    Added generate_routes

---
+++ local_search_helpers.py
@@ -0,0 +1,36 @@
+"""
+local_search_helpers.py
+
+Contains helper functions for Simmulated Annealing Local Search
+
+  - stop_placement(start, goal, num_stops) -> list of stops (including start & goal)
+  - generate_routes(stops) -> list of nodes/edges representing full movement sequence
+  - score_calculator(route) -> numeric score (lower is better)
+
+"""
+
+import networkx as nx
+
+def generate_routes(stop_sequence, graph):
+    resulting_route = []
+
+    for i in len(stop_sequence) - 1:
+        from_point = stop_sequence[i]
+        to_point = stop_sequence[i+1]
+
+        ## Get a path between two points from graph 
+        from_to_path = nx.shortest_path(graph, from_point, to_point, 'weight')
+
+        for j in len(from_to_path) - 1:
+            from_stop, to_stop = from_to_path[j], from_to_path[j + 1]
+            edge = graph.get_edge_data(from_stop, to_stop)
+
+            if len(resulting_route) == 0:
+                resulting_route.append(from_stop)
+
+            # Use the segment id to represent the edge - can change if needed
+            resulting_route.append(edge['segment_id'])
+            
+            resulting_route.append(to_stop)
+
+    return resulting_route
