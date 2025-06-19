a99bd9bcd892743ee2d52afaef8ec48a05a7e329
lee0123y <145239152+lee0123y@users.noreply.github.com>
2025-06-19 02:31:47 -0400

    Update a_star.py

--- a_star.py
+++ a_star.py
@@ -7,23 +7,36 @@ def a_star(graph, start, goal, heuristics):
     Uses heapq library as it is efficient and fast. 
     # https://www.geeksforgeeks.org/python/heap-queue-or-heapq-in-python/ 
     # https://docs.python.org/3/library/heapq.html
+    # https://www.redblobgames.com/pathfinding/a-star/introduction.html <- came_from
+
+    # Note: our first approach for A star was storing full path in each node, 
+    but that design added computation time. Therefore, we optimized it by 
+    storing f_score and current node in the queue and reconstructing the path 
+    at the end using came_from dictionary that tracks the previous node for each visited node.
     """
     q = []
-    visited = set()
 
     # initialize queue
-    # (g(n) = c(n) + h(n), c(n) = cost, current node, path so far)
-    HQ.heappush(q, ((0+heuristics(start, goal)), 0, start, [start]))
+    # (g(n) = c(n) + h(n), current node)
+    HQ.heappush(q, ((0+heuristics(start, goal)), start))
+    came_from = dict()
+    cost_dict = {start:0}
+
     while len(q)>0:
-        g, c, current, path = HQ.heappop(q)
-        if current in visited:
-            continue
+        g, current = HQ.heappop(q)
+
         if current == goal:
-            return path, c
+            path = [current]
+            while current in came_from:
+                current = came_from[current]
+                path.append(current)
+            return path[::-1], cost_dict[goal]
         for neighbor in graph.neighbors(current):
-            if neighbor in visited:
-                continue
             edge = graph.get_edge_data(current, neighbor)
             distance = edge.get("length", 1)
-            new_c = c + distance
-            HQ.heappush(q, (new_c + heuristics(neighbor, goal), new_c, neighbor, path + [neighbor]))
+            new_c = cost_dict[current] + distance
+            if neighbor not in cost_dict or new_c < cost_dict[neighbor]:
+                came_from[neighbor] = current
+                cost_dict[neighbor] = new_c
+                g_score = new_c + heuristics(neighbor, goal)
+                HQ.heappush(q, (g_score, neighbor))
